aebc8c701f86684aa1e41a542794bdc2d720b579
杀阡陌 <fangqin.duan@ideacome.com>
2021-08-13 10:36:23 +0800

    学习断言

---
+++ src/main/java/AssertTest.java
@@ -0,0 +1,15 @@
+import org.springframework.util.Assert;
+
+/**
+ * @Author: GodChin
+ * @Date: 2021/8/12 16:40
+ * @Blog: http://itdfq.com
+ * @QQ: 909256107
+ * @Description:
+ */
+public class AssertTest {
+    public static void main(String[] args) {
+        Integer age =null;
+        Assert.notNull(age,"age不能为空");
+    }
+}
