0f0aad097dda41673a798051501e01faf1a794b8
杀阡陌 <fangqin.duan@ideacome.com>
2021-08-17 10:49:57 +0800

    学习RSA加密解密算法

---
+++ HuToolTest/src/main/java/Test/Test1.java
@@ -0,0 +1,17 @@
+package Test;
+
+import cn.hutool.core.util.ReflectUtil;
+
+
+/**
+ * @Author: GodChin
+ * @Date: 2021/8/16 15:23
+ * @Blog: itdfq.com
+ * @QQ: 909256107
+ * @Description:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        RuoTest ruoTest = ReflectUtil.newInstance("RuoTest");
+    }
+}
