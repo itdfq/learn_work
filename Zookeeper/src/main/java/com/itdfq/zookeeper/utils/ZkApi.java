56414059c447c86dfe513d61b24b01767e5c3f0a
杀阡陌 <fangqin.duan@ideacome.com>
2021-10-25 11:16:38 +0800

    zk配置工具

---
+++ Zookeeper/src/main/java/com/itdfq/zookeeper/utils/ZkApi.java
@@ -0,0 +1,15 @@
+package com.itdfq.zookeeper.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: QianMo
+ * @Date: 2021/10/25 10:58
+ * @Description:
+ */
+@Component
+@Slf4j
+public class ZkApi {
+
+}
---
+++ Zookeeper/src/test/java/com/itdfq/zookeeper/ZookeeperApplicationTests.java
@@ -0,0 +1,33 @@
+package com.itdfq.zookeeper;
+
+import com.alibaba.fastjson.JSON;
+import com.itdfq.zookeeper.config.ZkConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Properties;
+
+@SpringBootTest
+class ZookeeperApplicationTests {
+
+    @Autowired
+    private ZkConfig zkConfig;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void get(){
+        String test = zkConfig.getProperties("project.name");
+        System.out.println(test);
+    }
+
+    @Test
+    public void  set() throws Exception {
+        zkConfig.setProperties("project.name","spring.application.name=Zookeeper123更新数据");
+    }
+
+
+}
